190f0be7e1512c18aee03f4db2aee3e73f94751f
Jana Biermann <xbieja@med.sak.local>
2017-06-13 15:19:53 +0200

    deleted old comments

--- project.py
+++ project.py
@@ -11,14 +11,16 @@ file = open(in_file, "r")
 #open csv file
 table = csv.reader(file, delimiter=',')
 
+#counter for gain, loss, neutral
 countMin = [0] * 10
 countZero = [0] * 10
 countPlus = [0] * 10
 
+
 d = 0
 for gene in table:
     i = 0
-    if d > 0:
+    if d > 0: #skip first row of table
         for patient in gene:
             if len(patient) < 3:
                 if patient == '-1':
@@ -29,32 +31,8 @@ for gene in table:
                     countPlus[i] += 1
                 i += 1
     d += 1
+
         
 print(countMin)
 print(countZero)
 print(countPlus)
-                
-                
-            
-
-# for tum in len(table):
-#     for row in table:
-#         tumour = row[tum]
-#         
-    
-
-
-
-
-#count all 1, -1, 0 
-#def count(table):
-    
-    # if i == 1:
-    #     gain = gain +1
-    # elif i == -1:
-    #     loss = loss +1
-    # elif i == 0:
-    #     neutral = neutal +1
-    # else:
-    #    #ignore
-    # return 
